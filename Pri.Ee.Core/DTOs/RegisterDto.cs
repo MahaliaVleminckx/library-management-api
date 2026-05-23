a68d38762b893d7cfd23925e19a88cfc98033602
MahaliaVleminckx <mahalia.vleminckx@student.howest.be>
2026-05-23 22:32:28 +0200

    Fix verification issues

--- Pri.Ee.Core/DTOs/RegisterDto.cs
+++ Pri.Ee.Core/DTOs/RegisterDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,12 +9,19 @@ namespace Pri.Ee.Core.DTOs
 {
     public class RegisterDto
     {
+        [Required]
         public string Username { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        
+        [Required]
         public DateTime BirthDate { get; set; }
     }
 }
